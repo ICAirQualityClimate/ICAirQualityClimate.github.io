141c3461ca335db8f2b8df4b3b2bf749622fe5de
ICAirQuailityClimate <ICAirQuailityClimate@gmail.com>
2022-03-11 13:31:55 -0500

    Edited autoGraph.bat and index.html

--- autoGraph.bat
+++ autoGraph.bat
@@ -1,9 +1,8 @@
 @echo off
+cd C:\Users\icairqualityclimate\Desktop\ICAirQualityClimate.github.io
 python "graph_script.py"
 
 git add Sensor_plot.png
 git add sensor_data.txt
 git commit -m "Updated graph and data"
 git push
-
-pause
